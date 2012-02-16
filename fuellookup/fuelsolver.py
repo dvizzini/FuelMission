d89614ed605cd4273ca7c514568cc646bed2b9c4
Your Name <jackdreilly@gmail.com>
2012-02-16 01:57:47 -0800

    all is ready to rock and roll

--- fuellookup/fuelsolver.py
+++ fuellookup/fuelsolver.py
@@ -1,33 +1,69 @@
 from cvxpy import *
-
 from geopy import distance
 from geopy import geocoders
-
+import time
 
 class Route:
     def __init__(self,stations):
 	self.stations = stations
+        self.set_distances()
 
+    def set_distances(self):
+        self.distances = [self.get_distance(first_stat,
+                                            second_stat)
+                                            for first_stat,second_stat
+                                            in zip(self.stations[:-1],
+                                                   self.stations[1:])]
+    
+    def get_distance(self,st1, st2):
+        try:
+            print st1
+            print st2
+            g = geocoders.Google()
+            _, d1 = g.geocode(st1.address)
+            _, d2 = g.geocode(st2.address)
+            max_val =  distance.distance(d1, d2).mi
+        except:
+            max_val = 5.
+        time.sleep(.25)
+        if max_val < 2.:
+            return 2.
+        return max_val
+
+    def distance_profile(self):
+        dist = 0
+        prof = [dist]
+        for distance in self.distances:
+            dist+=distance
+            prof.append(dist)
+        return prof
 
 
 class Car:
-    DEF_ECONOMY = 1.
-    DEF_START = 100.
-    def __init__(self, start = DEF_START, economy = DEF_ECONOMY):
+    DEF_ECONOMY = 10.
+    DEF_START = 80.
+    DEF_TANK = 100.
+    def __init__(self, 
+                 start=None,
+                 economy = None,
+                 tank_max = None):
+        if start is None:
+            start = self.DEF_START
+        if economy is None:
+            economy = self.DEF_ECONOMY
+        if tank_max is None:
+            tank_max = self.DEF_TANK
+            
 	self.start = start
 	self.economy = economy
+        self.tank_max = tank_max
 
-
-def distances(st1, st2):
-    g = geocoders.Google()
-    _, d1 = g.geocode(st1.address)
-    _, d2 = g.geocode(st2.address)
-    return distance.distance(d1, d2)
+            
 
         
 class FuelSolver:
 
-    def __init__(self, car, route, gas_min = 5.):
+    def __init__(self, car, route, gas_min = 10.):
 	self.car = car
 	self.route = route
 	self.gas_min = gas_min
@@ -35,30 +71,28 @@ class FuelSolver:
 
     def solve(self):
 	n = len(self.route.stations)
-	in_tank = variable(n+1,name ='gas in tank')
-	bought = variable(n+1,name = 'gas bought')
-	constraints = ([eq(bought[0],self.car.start)] +
-		       [eq(next_tank,
-			   prev_tank + prev_gas - 
-			   self.car.economy*distances(prev_stat, next_stat)
-			   ) 
-			   for prev_tank, 
-			       next_tank, 
-			       prev_stat, 
-			       next_stat, 
-			       prev_gas
-			       in zip(in_tank[:n],
-				      in_tank[1:],
-				      self.route.stations[:n],
-				      self.route.stations[1:],
-				      in_tank[:n])] +
-			 [ge(g,self.gas_min)])
-	p = program(minimize(sum([station.regular*stat_bought 
-				  for station,
-				      stat_bought 
-				      in zip(self.stations,
-					     bought)])),
-		    constraints)
+	in_tank = variable(n,name ='gas in tank')
+	bought = variable(n,name = 'gas bought')
+	constraints = ([eq(in_tank[0,0],self.car.start)] +
+		       [eq(in_tank[i+1,0],
+                           in_tank[i,0] +
+                           bought[i,0] -
+                           self.route.distances[i]*self.car.economy)
+                            for i in range(n-1)] + 
+                        [geq(in_tank,self.gas_min)] + 
+                        [leq(in_tank + bought,self.car.tank_max)] +
+                        [geq(bought,0)]
+                        )
+        fuel_cost = sum(
+            [self.route.stations[i].regular*
+             bought[i,0]
+             for i in range(n)]
+             )
+        print fuel_cost
+	p = program(minimize(fuel_cost),
+                             constraints)
 	p.solve()
-	return [b.value() for b in bought]
+        self.bought = bought
+        self.in_tank = in_tank
+        self.program = p        
 	
--- fuellookup/test.py
+++ fuellookup/test.py
@@ -4,6 +4,37 @@ from fuellookup import gas_prices
 from fuelsolver import *
 
 
-stations = gas_prices(95628)
 
-print distances(stations[0],stations[1])
+zip_code = 94709
+route = Route(gas_prices(zip_code)[:15])
+car = Car()
+
+solver = FuelSolver(car,route)
+solver.solve()
+
+
+def plot_data():
+    from pylab import *
+    bought = solver.bought.value.transpose().tolist()[0]
+    in_tank = solver.in_tank.value.transpose().tolist()[0]
+    prices = [station.regular for station in route.stations]
+    profile = route.distance_profile()
+    hold(True)
+    plot(profile,bought)
+    plot(profile,in_tank,'k--')
+    ylabel('amount of gas')
+    legend(['bought','in tank'],loc='upper left')
+    twinx()
+    plot(profile,prices,'r-.')
+    legend(['prices'],loc='upper right')
+    show()
+    xlabel('distance')
+    ylabel('fuel price')
+
+solver.program.show()
+print
+print
+print
+print 'buy values:'
+print solver.bought.value
+
